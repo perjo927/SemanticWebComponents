bdfbfbca8aee9b375ecc45c00d79b1b0fbd9b671
perjo927 <perjo927@student.liu.se>
2015-10-31 23:35:32 +0100

    Struggle ...

---
+++ src/core/bootstrap2.ts
@@ -0,0 +1,25 @@
+/*jshint browser:true */
+'use strict';
+
+/*
+    bootstrap.ts
+    Bootstraps Angular, rather than adding ng-app="app" into the html.
+*/
+
+// Load all vendor dependencies
+import "./vendor";
+
+// Load the main app file (src/app.ts)
+import appModule = require('../app');
+
+// Replaces ng-app="appName"
+angular.element(document).ready(function () {
+    // appModule.name will be taken from index.js, whatever its name might be:
+    // angular.module('THISNAMEHERE', []).
+    angular.bootstrap(document, [appModule.name], {
+        //strictDi: true
+    });
+});
+
+// require(./vendor')(); // run an empty function
+// var appModule = require('../index');
---
+++ src/core/vendor2.ts
@@ -0,0 +1,13 @@
+/* JS */
+// Module load Angular
+// Replaces adding <script src="bower_components/angular/angular.min.js">.
+import angular = require("angular");
+
+/* Styles */
+//import scss = require("../app.scss");
+//import css = require("../test.css");
+//import "../test.css";
+
+
+//export = { angular, scss, css };
+export = { angular };
